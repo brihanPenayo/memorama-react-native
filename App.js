a847f8f26b9530c5ee53e3e3d44bbd6ba045f617
Brihan Penayo <brihan.penayos@gmail.com>
2023-07-05 12:55:17 -0400

    Agregando estilos y lógica, juego funcional

--- App.js
+++ App.js
@@ -1,32 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import Cards from './Cards';
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 const emojis = [
   "🐶",
   "👀",
   "😻",
-  "🤖",
-  "👾",
+  // "🤖",
   "👻",
   "🙉",
   "💩",
-  "😄",
+  // "😄",
 ]
 
 export default function App() {
-  const [board, setBoard] = useState(()=>shuffleArray([...emojis, ...emojis]))
+  const [board, setBoard] = useState(() => shuffleArray([...emojis, ...emojis]))
+  const [selected, setSelected] = useState([])
+  const [matched, setMatched] = useState([])
+  const [movement, setMovement] = useState(0)
+
+  const winner = () => matched.length === board.length;
+  useEffect(() => {
+    if (selected.length < 2) return;
+    if (board[selected[0]] === board[selected[1]]) {
+      setMatched([...matched, ...selected])
+      setSelected([]);
+    } else {
+      const timeOut = setTimeout(() => setSelected([]), 1000)
+      return () => clearTimeout(timeOut);
+    }
+  }, [selected])
+
+  const handlePress = (index) => {
+    if (selected.length >= 2 || selected.includes(index)) return;
+    setSelected([...selected, index]);
+    setMovement(movement + 1)
+  }
+
+  const reset = () => {
+    setMatched([]);
+    setMovement(0);
+    setSelected([]);
+    setBoard(shuffleArray([...emojis, ...emojis]))
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Memorama</Text>
+      <Text style={styles.movement}>Movimientos: {movement}</Text>
       <View style={styles.viewSt}>
-      {board.map((card, index)=>{
-        return (
-          <Cards onPress={()=>alert("I like You and Tora, and Mochis")} key={index}>{card}</Cards>
-        )
-      })}
+        {board.map((card, index) => {
+          const turnedOver = selected.includes(index) || matched.includes(index)
+          return (
+            <Cards
+              key={index}
+              onPress={() => handlePress(index)}
+              turnedOver={turnedOver}>
+              {card}
+            </Cards>
+          )
+        })}
       </View>
       <StatusBar style="light" />
+      <Pressable style={styles.btn} onPress={reset}>
+        <Text style={styles.btnText}>Reiniciar</Text>
+      </Pressable>
     </View>
   );
 }
@@ -43,21 +81,36 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: 800
   },
+  btnText: {
+    fontWeight: 600,
+    color: 'white',
+    fontSize: 16
+  },
+  btn: {
+    backgroundColor: 'gray',
+    padding: 20,
+    borderRadius: 20,
+    width: 120,
+    alignItems: 'center'
+  },
   viewSt: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: 40,
+    margin: 40,
     gap: 10,
-    alignContent:'center',
     justifyContent: 'center'
+  },
+  movement: {
+    fontSize: 20,
+    color: 'white',
+    marginTop: 20
   }
 });
 
-  function shuffleArray(array) {
-    const newArray = [...array];
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-    }
-    return newArray;
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
   }
+  return array;
+}
--- Cards.js
+++ Cards.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import { Pressable, Text, StyleSheet } from 'react-native'
 
-const Cards = ({onPress, children, turnedOver}) => {
+const Cards = ({ onPress, children, turnedOver }) => {
   return (
-    <Pressable onPress={onPress} style={styles.card}>
-      <Text style={styles.emoji}>{children}</Text>
+    <Pressable onPress={onPress} style={[styles.card, (turnedOver ? styles.cardShow : styles.cardHide)]}>
+      {turnedOver ?
+        (<Text style={styles.emoji}>{children}</Text>)
+        :
+        (<Text style={styles.emoji}>?</Text>)}
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
     width: 100,
     height: 100,
-    borderRadius: 20
+    borderRadius: 20,
+    borderWidth: 10,
+    backgroundColor: 'white'
+  },
+  cardShow: {
+    backgroundColor: 'white',
+    borderColor: 'purple'
   },
   emoji: {
     fontSize: 40,
+    color: '#26402d'
+  },
+  cardHide: {
+    borderColor: 'gray'
   }
 });
 
